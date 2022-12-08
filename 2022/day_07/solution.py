a743f77fc035a5371afd53c5b7b9d5e21936f3d7
Katie Redderson-Lear <katie.reddersonlear@zapier.com>
2022-12-07 17:35:10 -0800

    day 7 pt 1 proper

--- 2022/day_07/solution.py
+++ 2022/day_07/solution.py
@@ -1,88 +1,62 @@
-with open('./input_test.txt') as f:
+with open('./input.txt') as f:
     # strip whitespace, put in new array
     contents = [line.strip() for line in f.readlines()]
 
-# if you see $ cd that's the current directory name
-# if you see ls, start iterating till there's another $ cd
+class Dir:
+    def __init__(self, name, parent):
+        self.name = name
+        self.parent = parent
+        self.files = {}
+        self.dirs = {}
 
-# do we even care if there's a `dir`?
-# yes i think we do because that tells us what to add up
-
-def add_dir_size_and_remove(list_of_dirs, final_sizes):
-    # this should return a list of dirs with one/more removed, and a size to add
-    size_sum = 0
-    for dir in list_of_dirs:
-        if dir in final_sizes:
-            size_to_add = final_sizes.get(dir)
-            size_sum += size_to_add
-            list_of_dirs.remove(dir)
-    return {
-        'new_list': list_of_dirs,
-        'size_sum': size_sum
-    }
-
-def find_empty_dirs(initial_map, final_sizes):
-    for key, value in initial_map.items():
-        if len(value['contents_dirs']) == 0:
-            final_sizes[key] = value['size']
-        else:
-            new_dir_info = add_dir_size_and_remove(value['contents_dirs'], final_sizes)
-            value['contents_dirs'] = new_dir_info['new_list']
-            value['size'] += new_dir_info['size_sum']
-    return {
-        'new_map': initial_map,
-        'new_final_sizes': final_sizes
-    }
+    @property
+    def final_sizes(self):
+        total = sum(self.files.values())
+        for dir in self.dirs.values():
+            total += dir.final_sizes
+        return total
 
 def initial_map_setup(list_of_lines):
-    filemap_output = {}
+    filemap_output = []
+    # start with /
+    current_directory = Dir('root', None)
+    filemap_output.append(current_directory)
+
     for line in list_of_lines:
         # it's just always useful to split it
         line_list = line.split()
+        # ignore the first one
+        if line == '$ cd /': continue
         # if it's a command
-        if line_list[0] == '$':
+        elif line_list[0] == '$':
             # if it's a cd
             if line_list[1] == 'cd':
                 if line_list[-1] == '..':
-                    # TODO
-                    pass
+                    current_directory = current_directory.parent
                 else:
-                    # add that dir name to the filemap
-                    current_directory = line_list[-1]
-                    filemap_output[current_directory] = {}
-                    filemap_output[current_directory]['size'] = 0
-                    filemap_output[current_directory]['contents_dirs'] = []
-                    filemap_output[current_directory]['contents_files'] = []
-            # if it's ls
+                    # update current directory to one inside it
+                    current_directory = current_directory.dirs[line_list[-1]]
             elif line_list[1] == 'ls':
-                # i think this is important because it tells us what we need to add up??
-                # TODO
-                pass
+                continue
         # if it's a dir
         elif line_list[0] == 'dir':
-            # TODO
-            filemap_output[current_directory]['contents_dirs'].append(line_list[1])
+            # create a dir with that dir name and its parent
+            new_dir = Dir(line_list[1], current_directory)
+            # add it to the dirs of current_directory
+            current_directory.dirs[new_dir.name] = new_dir
+            # add it to the list
+            filemap_output.append(new_dir)
         # if we get here, it's a file
         else:
-            filemap_output[current_directory]['contents_files'].append(line_list[1])
-            filemap_output[current_directory]['size'] += int(line_list[0])
-            # we gotta clear the list of contents at some point...
+            current_directory.files[line_list[1]] = int(line_list[0])
     return filemap_output
 
 def main(lines):
     map_dict = initial_map_setup(lines)
-    print(map_dict)
-    # map_dict = {'d': {'size': 24933642, 'contents_dirs': [], 'contents_files': ['j', 'd.log', 'd.ext', 'k']}}
-    final_sizes = {}
-    while map_dict.keys() != final_sizes.keys():
-        new_info = find_empty_dirs(map_dict, final_sizes)
-        map_dict = new_info['new_map']
-        final_sizes = new_info['new_final_sizes']
-
-    solution_list = []
-    for value in final_sizes.values():
-        if value <= 100000:
-            solution_list.append(value)
-    print(solution_list)
+    solution = 0
+    for item in map_dict:
+        if item.final_sizes <= 100000:
+            solution += item.final_sizes
+    print(solution)
 
 main(contents)
